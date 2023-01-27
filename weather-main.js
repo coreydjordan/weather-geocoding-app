91dd1236fe29a1221baf7ec736eba730299621b9
coreydjordan <83131355+coreydjordan@users.noreply.github.com>
2023-01-26 23:21:50 -0600

    added logic to check if city entered is in the US or not. Set to show mph or kpm based on city location.

--- weather-main.js
+++ weather-main.js
@@ -1,13 +1,11 @@
-//figure out how to check if city uses mph or kph and if they use C or F. Figure out how to ask for location and get weather based on that.
-
-
 var inputval = document.querySelector('#cityinput');
 var btn = document.querySelector('#add');
+var butn = document.querySelector('#butn')
 var city = document.querySelector('#cityoutput');
 var descrip = document.querySelector('#description');
 var temp = document.querySelector('#temp');
 var wind = document.querySelector('#wind');
-
+var country = document.querySelector('#country');
 
 apik = "3045dd712ffe6e702e3245525ac7fa38"
 
@@ -20,14 +18,25 @@ function convertion(val){
 function feels_conv(val){
     return ((val - 273.15)*9/5 + 32).toFixed(0);
 };
-function km_conv(val){
-    return val / 1.609.toFixed(0);
-    
-    
+function km_conv(val, cntry){
+    if (cntry != "US"){
+        return val + ' kph'
+    }else{
+    return val / 1.609.toFixed(0) + ' mph';
+}
+}
+
+function country_name(cntry){
+    if (cntry == "US"){
+        return "You're in the US"
+    }else{
+        return "You are not in the US"
+    }
 }
     convertion();
     feels_conv();
     km_conv();
+    country_name();
 
 btn.addEventListener('click', function()
 {
@@ -39,11 +48,13 @@ btn.addEventListener('click', function()
         var tempature = data['main']['temp'];
         var wndspeed = data['wind']['speed'];
         var feels_like = data['main']['feels_like'];
+        var cntry = data['sys']['country'];
         city.innerHTML = `Weather of <span>${namevalue}</span>`;
         temp.innerHTML = `Temperature: <span>${convertion(tempature)}&#8457</span>`;
         description.innerHTML = `Sky Conditions: <span>${descrip}</span>`
-        wind.innerHTML = `Wind speed: <span>${km_conv(wndspeed)} mph</span>`
-        feelslike.innerHTML = `Feels like: <span>${feels_conv(feels_like)}&#8457</span>`
+        wind.innerHTML = `Wind speed: <span>${km_conv(wndspeed, cntry)}</span>`
+        feelslike.innerHTML = `Feels like: <span>${feels_conv(feels_like)}&#8457</span>`;
+        country.innerHTML = `Country:<span> ${country_name(cntry)}</span>`;
     })
     
     .catch(err => alert('You entered a wrong city name'))
